6abd5219fc4adfcf81781415572ad02558794ffd
Hal27000 <alessiohuma@tutanota.com>
2022-04-22 22:03:05 +0200

    task-tracker-commit

---
+++ angular-task-tracker/src/app/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id?: number; //initially optional so that when we initially create the taks in the json server it's not going to give back an error
+    text: string;
+    day: string;
+    reminder: boolean;
+  }
---
+++ angular-task-tracker/src/app/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  title: string = 'Task tracker';
+  showAddTask!:boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService:UiService) { }
+
+  ngOnInit(): void {
+  }
+
+  toggleAddTask(){
+    this.uiService.toggleAddTask();
+  }
+
+}
